f7998f2fb39d694bd820f6057fc38d99b84110c0
Krzysztof Gądek <krzysztof.gadek15@gmail.com>
2020-09-09 23:11:19 +0200

    database 4

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -22,21 +22,21 @@ class MainController extends AbstractController
         $entityManager = $this->getDoctrine()->getManager();
         for($i = 0; $i < 1000; $i++){
             $book = new Books();
-            $categories = array("rodzinne","imprezowe","serialowe","dla dzieci","erotyczne");
-            $title = "Przykladowa nazwa gry ".$i;
-            $desc = "Początek traktatu czasu być cnotliwym lub przyrachowane. W przyczytaniu musi być mogą. Te ostatnie zostawił to Pan Żochowski w roku życia takich skoropisów utworzyło się uczynku nastąpić ma. Najwyższa zasada prawodawstwa koniecznie po boru a wszakże Dobro więc wkradło się różne predykaty czyli pobudkę do złego przez rychłą pokutę wszelkich religiów, podporą i dla tego wyłączyć, ponieważ mu dał mu się przyszło dorozumiewał ";
-            $author = "-";
+            $categories = array("romans","literatura piekna","kryminal","fantasy","przygodowa");
+            $title = "Przykladowy tytul ksiazki ".$i;
+            $desc = "Litwo! Ojczyzno moja! Ty jesteś jak zdrowie. Nazywał się w końcu dzieje chciano zamknąć w moim dom i z Bonapartą. tu świeccy, do marszu! Pójdziem, czy moda odmieniła z opieki nie wyszli witać, ale nic - nowe dziwo w której wytryskał rumieniec, ilekroć z dziecinną radością pociągnął za stołem siadał i zalety Ściągnęły wzrok stryja ku drzwiom odprowadzał i zgasło. I tak rzadka ciche grusze siedzą. Śród takich pól malowanych zbożem rozmaitem wyzłacanych ";
+            $author = array("Adam Mickiewicz", "Napoleon Bonaparte","Bolesław Prus","J.K.Rowling","Grzegorz Brzęczyszczykiewicz");
             $price = rand(10,100);
             $img_src = rand(1,10);
             $year = rand(2013,2020);
             $month = rand(1,12);
             $day = rand(1,28);
-            $book->setAuthor($author);
+            $book->setAuthor($author[rand(0,4)]);
             $book->setSubcategory($categories[rand(0,4)]);
             $book->setDescribtion($desc);
             $book->setPrice($price);
             $book->setDateOfAdding(\DateTime::createFromFormat('Y-m-d', $year."-".$month."-".$day));
-            $book->setCategory("gra");
+            $book->setCategory("ksiazka");
             $book->setImageSrc($img_src);
             $book->setTitle($title);
             $entityManager->persist($book);
