267e6733440b2b2251bb176d1a1d0b71c7363af6
Krzysztof Gądek <krzysztof.gadek15@gmail.com>
2020-09-09 23:01:50 +0200

    database 3

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -20,10 +20,10 @@ class MainController extends AbstractController
             $login = $_SESSION['login'];
         }
         $entityManager = $this->getDoctrine()->getManager();
-        /*for($i = 0; $i < 1000; $i++){
+        for($i = 0; $i < 1000; $i++){
             $book = new Books();
             $categories = array("rodzinne","imprezowe","serialowe","dla dzieci","erotyczne");
-            $title = "Przykladowa nazwa  ".$i;
+            $title = "Przykladowa nazwa gry ".$i;
             $desc = "Początek traktatu czasu być cnotliwym lub przyrachowane. W przyczytaniu musi być mogą. Te ostatnie zostawił to Pan Żochowski w roku życia takich skoropisów utworzyło się uczynku nastąpić ma. Najwyższa zasada prawodawstwa koniecznie po boru a wszakże Dobro więc wkradło się różne predykaty czyli pobudkę do złego przez rychłą pokutę wszelkich religiów, podporą i dla tego wyłączyć, ponieważ mu dał mu się przyszło dorozumiewał ";
             $author = "-";
             $price = rand(10,100);
@@ -41,7 +41,7 @@ class MainController extends AbstractController
             $book->setTitle($title);
             $entityManager->persist($book);
             $entityManager->flush();
-        }*/
+        }
         $books = $this->getDoctrine()->getRepository(Books::class)->get4newest();
         return $this->render('index.html.twig',['logged' => $logged, 'login' => $login, 'books' => $books]);
     }
